bb2692bd29e7db4320b9b01c5c7e92a4e6736810
gabriel case <gcase@atlassian.com>
2019-09-13 18:16:16 -0500

    added code to check for error

--- maps/maps.go
+++ maps/maps.go
@@ -4,11 +4,13 @@ import "errors"
 
 type Content map[string]string
 
+var ErrNotFound = errors.New("word not found")
+
 func (c Content) Search(word string) (string, error) {
 	found := c[word]
 
 	if len(found) > 1 {
 		return found, nil
 	}
-	return "", errors.New("word not found")
+	return "", ErrNotFound
 }
