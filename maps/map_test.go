ff71cf85e365d29d3eec52c209ec9b89f831a9b4
gabriel case <gcase@atlassian.com>
2019-09-17 19:49:05 -0500

    added new unit tests for content update

--- maps/map_test.go
+++ maps/map_test.go
@@ -7,7 +7,6 @@ import (
 
 func TestSearch(t *testing.T) {
 	content := Content{"test": "this is just a test"}
-
 	t.Run("lookup a word that exists", func(t *testing.T) {
 		got, err := content.Search("test")
 		want := "this is just a test"
@@ -15,7 +14,6 @@ func TestSearch(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, want, got)
 	})
-
 	t.Run("lookup a word that does not exist", func(t *testing.T) {
 		got, err := content.Search("unknown")
 
@@ -25,13 +23,43 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	content := Content{}
-	content.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		content := Content{}
+		content.Add("test", "this is just a test")
 
-	want := "this is just a test"
-	got, err := content.Search("test")
+		want := "this is just a test"
+		got, err := content.Search("test")
 
-	assert.NoError(t, err)
+		assert.NoError(t, err)
+		assert.Equal(t, want, got)
+	})
+	t.Run("existing word", func(t *testing.T) {
+			word := "test"
+			definition := "this is just a test"
+			content := Content{word: definition}
+			err := content.Add(word, "new test")
 
-	assert.Equal(t, want, got)
+			assert.Errorf(t, err, ErrWordExists.Error())
+			assert.Equal(t, definition, content[word])
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		content := Content{word: definition}
+		newDefinition := "new definition"
+		content.Update(word, newDefinition)
+
+		assert.Equal(t, newDefinition, content[word])
+	})
+
+	t.Run("new word", func(t *testing.T) {
+			word := "test"
+			definition := "this is just a test"
+			content := Content{}
+			err := content.Update(word, definition)
+			assert.Errorf(t, err, ErrWordNotExists.Error())
+	})
 }
--- maps/maps.go
+++ maps/maps.go
@@ -1,10 +1,18 @@
 package maps
 
-import "errors"
-
 type Content map[string]string
 
-var ErrNotFound = errors.New("word not found")
+const (
+	ErrNotFound = ContentErr("word not found")
+	ErrWordExists = ContentErr("cannot add word because it already exists")
+	ErrWordNotExists = ContentErr("cannot update word because it does not exist")
+)
+
+type ContentErr string
+
+func (e ContentErr) Error() string {
+	return string(e)
+}
 
 func (c Content) Search(word string) (string, error) {
 	found := c[word]
@@ -15,6 +23,30 @@ func (c Content) Search(word string) (string, error) {
 	return "", ErrNotFound
 }
 
-func (c Content) Add(word string, definition string) {
-	c[word] = definition
+func (c Content) Add(word string, definition string) error{
+	_, err := c.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		c[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (c Content) Update(word, definition string) error{
+	_, err := c.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordNotExists
+	case nil:
+		c[word] = definition
+	default:
+		return err
+	}
+	return nil
 }
